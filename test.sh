5000b1167e7daa3aa78b7431d9e56b39d72a5d8b
Westporch <westporch@gmail.com>
2015-09-23 23:24:30 +0900

    test

--- test.sh
+++ test.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
-SOURCE_TXT=sniper-usr-bin.txt
-SOURCE_TXT_BAK=sniper-usr-bin.txt.bak
-TARGET_TXT=sniper-bin.txt
+SOURCE_TXT=new.txt
+SOURCE_TXT_BAK=new.txt.bak
+TARGET_TXT=old.txt
+NEW_CMDS_TXT=new-OS_cmds.txt
 RESULT_TXT=result.txt
 
 txt_lines=`cat $TARGET_TXT | wc -l`
 
-readarray -t s_arr < $TARGET_TXT #텍스트 파일의 내용을 배열에 저장한다.
+function create_cmd_txt()
+{
+	arr=("/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin")
+	
+	for ((idx=0; idx < ${#arr[@]}; idx++))
+	do
+		ls --color=no ${arr[$idx]} | sort >> $NEW_CMDS_TXT
+	done
+}
 
 
-cp $SOURCE_TXT $SOURCE_TXT_BAK  #원본 문서 백업
 
-for ((idx=0; idx<txt_lines; idx++))
-do
-    echo -e "${s_arr[$idx]}\n"
+function exclude_old_cmds()
+{
+	readarray -t s_arr < $TARGET_TXT #텍스트 파일의 내용을 배열에 저장한다.
 
-    sed -e "/${s_arr[$idx]}/d" $SOURCE_TXT > $RESULT_TXT
+	cp $SOURCE_TXT $SOURCE_TXT_BAK  #원본 문서 백업
 
-#   echo -e "\n" >> $RESULT_TXT
+	for ((idx=0; idx<txt_lines; idx++))
+	do
+    	echo -e "${s_arr[$idx]}\n"
 
-    yes | cp $RESULT_TXT $SOURCE_TXT
+    	sed -e "/${s_arr[$idx]}/d" $SOURCE_TXT > $RESULT_TXT
 
+		#   echo -e "\n" >> $RESULT_TXT
+
+    	yes | cp $RESULT_TXT $SOURCE_TXT
 done
+}
+
+create_cmd_txt
+exclude_old_cmds
 
