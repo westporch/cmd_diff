02122fb933d2fbd37af53e94275df86db6dc8339
Westporch <westporch@gmail.com>
2015-09-24 13:56:03 +0900

    최초 완성 쉘 스크립트

--- cmd_diff.sh
+++ cmd_diff.sh
@@ -16,56 +16,74 @@ RESULT_TXT=result.txt
 function create_cmd_txt()
 {
 
-	    #old OS의 디렉토리에 있는 명령어들을 하나의 텍스트 파일에 저장함.
-	    oldOS_dir_arr=("/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin")
+	#old OS의 디렉토리에 있는 명령어들을 하나의 텍스트 파일에 저장함.
+  	oldOS_dir_arr=("/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin")
 
-	    for ((idx=0; idx < ${#oldOS_dir_arr[@]}; idx++))
-	    do
-        	ls --color=no $SECOND_OS_DIR_PATH${oldOS_dir_arr[$idx]} | sort >> $OLD_CMDS_TXT_INCLUDE_Lbracket
-	    done
+    for ((idx=0; idx < ${#oldOS_dir_arr[@]}; idx++))
+    do
+		ls --color=no $SECOND_OS_DIR_PATH${oldOS_dir_arr[$idx]} | sort >> $OLD_CMDS_TXT_INCLUDE_Lbracket
 
- 	   #new OS의 디렉토리에 있는 명령어들을 하나의 텍스트 파일에 저장함.
- 	   newOS_dir_arr=("/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin")
+    done
 
- 	   for ((idx=0; idx < ${#newOS_dir_arr[@]}; idx++))
- 	   do
-	       ls --color=no ${newOS_dir_arr[$idx]} | sort >> $NEW_CMDS_TXT_INCLUDE_Lbracket
-	   done   
+	#new OS의 디렉토리에 있는 명령어들을 하나의 텍스트 파일에 저장함.
+	newOS_dir_arr=("/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin")
+
+	for ((idx=0; idx < ${#newOS_dir_arr[@]}; idx++))
+	do
+        ls --color=no ${newOS_dir_arr[$idx]} | sort >> $NEW_CMDS_TXT_INCLUDE_Lbracket
+
+	done	
 }
 
 function delete_left_bracket()
 {
-	    Lbracket_fname_arr=($OLD_CMDS_TXT_INCLUDE_Lbracket $NEW_CMDS_TXT_INCLUDE_Lbracket)
-	    fname_arr=($OLD_CMDS_TXT $NEW_CMDS_TXT)
+	Lbracket_fname_arr=($OLD_CMDS_TXT_INCLUDE_Lbracket $NEW_CMDS_TXT_INCLUDE_Lbracket)
+	fname_arr=($OLD_CMDS_TXT $NEW_CMDS_TXT)
 
-	    for ((idx=0; idx < ${#Lbracket_fname_arr[@]}; idx++))
-	    do
-	        sed -e "/\[/d" ${Lbracket_fname_arr[$idx]} > ${fname_arr[$idx]}
-	    done
+	for ((idx=0; idx < ${#Lbracket_fname_arr[@]}; idx++))
+	do
+		sed -e "/\[/d" ${Lbracket_fname_arr[$idx]} > ${fname_arr[$idx]}
+	done
 
-	    #Left bracket([)를 포함한 파일 삭제
-	    rm -rf $OLD_CMDS_TXT_INCLUDE_Lbracket $NEW_CMDS_TXT_INCLUDE_Lbracket
+	#Left bracket([)를 포함한 파일 삭제
+	rm -rf $OLD_CMDS_TXT_INCLUDE_Lbracket $NEW_CMDS_TXT_INCLUDE_Lbracket
 }
+
 
 function delete_old_cmds()
 {
-	    txt_lines=`cat $OLD_CMDS_TXT | wc -l`
-	    readarray -t s_arr < $OLD_CMDS_TXT #텍스트 파일의 내용을 배열에 저장한다.
+	txt_lines=`cat $OLD_CMDS_TXT | wc -l`
+    readarray -t s_arr < $OLD_CMDS_TXT #텍스트 파일의 내용을 배열에 저장한다.
 
-	    cp $NEW_CMDS_TXT $NEW_CMDS_TXT_PROCESSING #sed 작업할 문서 생성
+    cp $NEW_CMDS_TXT $NEW_CMDS_TXT_PROCESSING #sed 작업할 문서 생성
 
-	    for ((idx=0; idx<txt_lines; idx++))
-	    do
-	        echo -e "${s_arr[$idx]}\n"
+    for ((idx=0; idx<txt_lines; idx++))
+    do
+        echo -e "${s_arr[$idx]}\n"
 
-	        sed -e "/${s_arr[$idx]}/d" $NEW_CMDS_TXT_PROCESSING > $RESULT_TXT
+        sed -e "/${s_arr[$idx]}/d" $NEW_CMDS_TXT_PROCESSING > $RESULT_TXT
 
-	        yes | cp $RESULT_TXT $NEW_CMDS_TXT_PROCESSING
-	   done
+        yes | cp $RESULT_TXT $NEW_CMDS_TXT_PROCESSING
+	done
 
-	   rm -rf $NEW_CMDS_TXT_PROCESSING #작업 파일 삭제
+    rm -rf $NEW_CMDS_TXT_PROCESSING #작업 파일 삭제
 }
+
+function add_cmds_path()
+{
+	result_lines=`cat $RESULT_TXT | wc -l`
+	readarray -t result_arr < $RESULT_TXT
+
+	rm -rf $RESULT_TXT
+
+	for ((idx=0; idx<result_lines; idx++))
+	do
+		echo -e "${result_arr[$idx]}\t\t\t\t `whereis ${result_arr[$idx]} | awk -F " " '{print $2}'`" >> $RESULT_TXT
+	done
+}
+
 
 create_cmd_txt
 delete_left_bracket
 delete_old_cmds
+add_cmds_path
